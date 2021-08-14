5710a8ace66def9c0d86a713fe9dc5b17c129b02
Huidong Hwang <hwangh95@gmail.com>
2021-08-14 15:19:11 +0200

    Fix directory issues in scripts and config files

--- emvs.sh
+++ emvs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-bag_dir=`eval echo $1`
+bag_dir=$(realpath $1)
+script_dir=$(dirname $(realpath $0))
 if [ ! -d "${bag_dir}" ]; then
     echo "Please enter a valid bag path"
     exit 1
 fi
 
 for bag in ${bag_dir}/*.bag; do
-    rosrun mapper_emvs run_emvs --bag_filename=${bag} --flagfile=emvs_config.conf
+    rosrun mapper_emvs run_emvs --bag_filename=${bag} --flagfile=${script_dir}/emvs_config.conf
     base=$(basename ${bag} .bag)
     mv dsi.npy ${bag_dir}/${base}.npy
     mv pointcloud.pcd ${bag_dir}/${base}.pcd
--- esim.sh
+++ esim.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-bag_dir=$1
+bag_dir=$(realpath $1)
+script_dir=$(dirname $(realpath $0))
 
-if [ ! -d "bag_dir" ]; then
+if [ ! -d "${bag_dir}" ]; then
     echo "Please enter a valid bag path"
     exit 1
 fi
 
 for depth in 5 10
 do
-    sed -i "23s/.*/--renderer_plane_z=-${depth}/" esim_config.conf
+    sed -i "23s/.*/--renderer_plane_z=-${depth}/" ${script_dir}/esim_config.conf
 
     for baseline in {5..50..5}
     do
-        sed -i "4s|.*|--path_to_output_bag=${bag_dir}/${depth}mdepth_$(printf '0.%02d' ${baseline})baseline.bag|" esim_config.conf
-        sed -i "35s|.*|--trajectory_multiplier_x=$(printf '0.%03d' $(($baseline*5)))|" esim_config.conf
-        roslaunch esim_ros esim.launch config:=esim_config.conf &
+        sed -i "4s|.*|--path_to_output_bag=${bag_dir}/${depth}mdepth_$(printf '0.%02d' ${baseline})baseline.bag|" ${script_dir}/esim_config.conf
+        sed -i "14s|.*|--calib_filename=${script_dir}/camera_config.yaml|" ${script_dir}/esim_config.conf
+        sed -i "17s|.*|--renderer_texture=${script_dir}/rpg_esim/event_camera_simulator/imp/imp_planar_renderer/textures/rocks.jpg|" ${script_dir}/esim_config.conf
+        sed -i "35s|.*|--trajectory_multiplier_x=$(printf '0.%03d' $(($baseline*5)))|" ${script_dir}/esim_config.conf
+        roslaunch esim_ros esim.launch config:=${script_dir}/esim_config.conf &
         sleep 1
         tail --pid=$(pgrep esim_node) -f /dev/null
         pgrep -f optic_flow | xargs kill
